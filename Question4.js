197e970564c82d71db7e96b0dd3f770f39a391f7
nautiyaljyotirmay <jyotirmaynautiyal@gmail.com>
2026-04-08 22:24:50 +0530

    Create Question4.js

---
+++ Question4.js
@@ -0,0 +1,22 @@
+let base = parseInt(prompt("Enter base:"));
+let distance = parseInt(prompt("Enter distance:"));
+let minutesLate = parseInt(prompt("Enter minutesLate:"));
+let seed = parseInt(prompt("Enter seed:"));
+
+let fare = base + 7 * distance;
+
+if (minutesLate > 15) {
+    fare = fare + 20;
+}
+
+if (distance > 10) {
+    fare = fare + Math.floor(fare * 0.1);
+}
+
+if (seed % 2 === 1) {
+    fare = fare - seed;
+} else {
+    fare = fare + seed;
+}
+
+fare = Math.ceil(fare / 5) * 5;
